4c9a0cfbb52826aa1752867bc3d3eb2179192c20
hamed18 <hamedcnec@gmail.com>
2024-09-04 03:33:45 +0600

    CREATE api. Add Food UI form data stored in database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
@@ -24,6 +24,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+	// CREATE: send user input data from server to database. client: Add Spot 
+	const AvailableFoodCollection = client.db("FoodSharing").collection("AvailableFoods");
+	app.post('/addFoods', async(req,res) => {
+		const newFood = req.body;
+		console.log(newFood);
+		const result = await AvailableFoodCollection.insertOne(newFood);
+		res.send(result);
+	})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
